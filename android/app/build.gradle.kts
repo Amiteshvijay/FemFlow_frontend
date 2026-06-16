bc22aeb27462cddebf8549137cf9443ed2a1c8b6
Amitesh Tiwari <amitesh3401@gmail.com>
2026-06-17 01:24:20 +0530

    build: bump version to 7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0.0"
         multiDexEnabled = true
     }
